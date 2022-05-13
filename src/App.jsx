de936f8feba3144c35146875c432aebb9e6cc26f
losrobbos <losrobbos@web.de>
2022-05-13 14:58:44 +0200

    Implemented PersonA component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import PersonA from './components/PersonA'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       <header className="App-header">
         <h2>Geile Team App</h2>
+        <PersonA />
       </header>
     </div>
   )
