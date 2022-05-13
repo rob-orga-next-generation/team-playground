830b7005c803c55e30ae9bb8a336276d35017643
losrobbos <losrobbos@web.de>
2022-05-13 15:04:08 +0200

    Implemented PersonB

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import PersonA from './components/PersonA'
+import PersonB from './components/PersonB'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
       <header className="App-header">
         <h2>Geile Team App</h2>
         <PersonA />
+        <PersonB />
       </header>
     </div>
   )
